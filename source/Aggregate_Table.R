c820da0bbc9dfedadedb038578c40a2c5f548171
Gsr19 <97632382+Gsr19@users.noreply.github.com>
2022-02-18 00:07:43 -0800

    Added intial Aggregate Table

---
+++ source/Aggregate_Table.R
@@ -0,0 +1,28 @@
+DT2020 <- read.csv("~/Desktop/final-project-starter-Gsr19/data/2020_LA_Crime_Dataset.csv")
+DT2021 <- read.csv("~/Desktop/final-project-starter-Gsr19/data/2021_LA_Crime_Dataset.csv")
+
+library(dplyr)
+
+Combined <- bind_rows(DT2020, DT2021)
+
+Chopped <- select(Combined, "AREA.NAME", "Crm.Cd.Desc", "Weapon.Desc")
+Chopped <- rename(Chopped, District = "AREA.NAME", Crime = "Crm.Cd.Desc", WeaponUsed = "Weapon.Desc")
+Chopped <- group_by(Chopped, District)
+
+Agg_Table <- table(Chopped$Crime, Chopped$District)
+
+NonViol <- filter(Chopped, TRUE == is.na(WeaponUsed))
+NonViolAgg_Table <-table(NonViol$Crime, NonViol$District)
+
+Viol <- filter(Chopped, FALSE == is.na(WeaponUsed))
+ViolAgg_Table <- table(Viol$Crime, Viol$District)
+
+#I Included three tables one for the total frequency of each crime in a district,
+#One that only shows the frequency of Non-Violent crimes in a district,
+#And one that only shows the Violent crime frequency in a district
+#
+#These tables help reveal the areas where Violent crime is persistent compared 
+#to non-violent crimes showing where police presence could help save lives
+#compared to areas mostly consisting of non-violent crime where police presence 
+#would likely only save property from being stolen in that area and would just 
+#drive people to steal from other areas.
